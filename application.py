ccc5d4bf80675aa65d61291a329a075d7f8d8079
Sofia Rytik <dv11@3soft.ru>
2022-05-31 19:29:53 +0300

    changed creaing window

--- application.py
+++ application.py
@@ -1,16 +1,23 @@
+from __future__ import annotations
+import pygame
 from window import Window
 from event import Event
 
 
 class Application:
     def __init__(self):
+        pygame.init()
         self.windows = []
         self.events = []
         self.is_running = False
+        self.active_window = None
 
     def event_loop(self):
-        for event in self.events:
-            event.emit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.stop()
+            else:
+                pass
 
     def run(self):
         self.is_running = True
@@ -22,3 +29,9 @@ class Application:
     def create_window(self, title):
         window = Window(title, self)
         self.windows.append(window)
+        if self.active_window is None:
+            self.active_window = window
+        return window
+
+    def stop(self):
+        self.is_running = False
--- window.py
+++ window.py
@@ -1,9 +1,19 @@
+import pygame
+
+
 class Window:
+    SCREEN_WIDTH = 800
+    SCREEN_HEIGHT = 800
+    Color = (0, 0, 0)
+
     def __init__(self, title, parent):
         self.widgets = []
         self.parent = parent
-        self.title = title
+        self.window = pygame.display.set_mode((Window.SCREEN_WIDTH, Window.SCREEN_HEIGHT))
+        pygame.display.set_caption(title)
 
     def render(self):
+        self.window.fill(Window.Color)
+        pygame.display.flip()
         for widget in self.widgets:
             widget.render()
